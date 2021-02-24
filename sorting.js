efb7501f5b804dedc11f2fb8186b0e17849a2a49
Josh <joshua.lee.mccombs@gmail.com>
2021-02-24 13:49:23 -0600

    new array button and functionality

--- sorting.js
+++ sorting.js
@@ -1,30 +1,56 @@
-const newDiv = document.createElement('div');
-const div1 = document.querySelector('.div1');
+// const newDiv = document.createElement('div');
+let newDiv, displayArr;
 const barWidth = 10;
 const barMainColor = '#5050aa';
 const barSecondaryColor = '#aa5050';
-const barContainer = document.querySelector('.div1');
+const barContainer = document.querySelector('.sorting-container');
+
+window.onload = function () {
+  init();
+  console.log('loaded')
+};
+
+const init = () => {
+  newDiv = document.createElement('div');
+  newDiv.setAttribute('class', 'sorting-box');
+  displayArr = initializeArray();
+  // newDiv.setAttribute('id', 'sorting-box');
+  barContainer.appendChild(newDiv);
+  displayArray();
+}
 
 const initializeArray = () => {
   let array = [];
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 60; i++) {
     array.push(Math.floor(Math.random() * 100) + 5);
   }
   return array;
 }
 
-const displayArr = initializeArray();
+// let displayArr = initializeArray();
 
-for (let num of displayArr) {
-  const barDiv = document.createElement('div', [`height: ${num}px`, 'width: 4px']);
-  barDiv.style.height = `${num}px`;
-  barDiv.setAttribute('class', 'array-item');
-  // barDiv.style.width = `${barWidth}px`;
-  barDiv.style.backgroundColor = barMainColor;
-  // barDiv.style.display = 'inline-block';
-  newDiv.appendChild(barDiv);
+let displayArray = () => {
+  for (let num of displayArr) {
+    const barDiv = document.createElement('div');
+    barDiv.style.height = `${num}px`;
+    barDiv.setAttribute('class', 'array-item');
+    barDiv.style.backgroundColor = barMainColor;
+    newDiv.appendChild(barDiv);
+  }
+  console.log('display')
 }
 
-// document.body.insertBefore(newDiv, div1);
-// document.body.appendChild(newDiv)
-barContainer.appendChild(newDiv);
+let cleanUp = () => {
+  let bars = document.getElementsByClassName('sorting-box');
+  while (bars.length > 0) {
+    bars[0].parentNode.removeChild(bars[0]);
+  }
+}
+
+
+const newArray = () => {
+  cleanUp();
+  console.log('clicked')
+  init();
+  // displayArray();
+}
